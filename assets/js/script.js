924e644f04a9c19c66357efc74c2692fdfc1d066
Jake Melville <jakemelville@ymail.com>
2021-02-16 16:23:34 -0500

    added styling using css and bootstrap. Created character cards for each avenger. Added trailer of first move from youtube. Created navbar which will eventually allow us to link to the indicual hero pages

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// http://www.omdbapi.com/?t=the+avengers&apikey=fbdde902
+
+
+// var searchBtn = document.querySelector(".btn");
+
+function seacrhWindow(event) {
+    event.preventDefault();
+
+    var formatInput = document.getElementById("search-input").value;
+
+    if (!formatInput) {
+        console.error('You need to enter characters/numbers');
+        return;
+      }
+    
+      var queryString = '' + formatInput;
+    
+      location.assign(queryString);
+}
+// searchBtn.addEventListener("click", seacrhWindow);
