7b99affa647fab3ed5b2694224002bc8ab431c42
dvaghani <dhruvi.vaghani@gmail.com>
2021-02-17 08:06:25 -0500

    saving questions & answers

---
+++ assets/js/quiz.js
@@ -0,0 +1,151 @@
+var triviaQuestions = [ {
+    question: "Who does HUlk call 'puny god' while throwing them around like a rag doll?",
+    options: [
+        "Thor",
+        "Loki",
+        "Captian America",
+        "Iron Man",
+    ],
+    answer: "Loki"
+},
+{
+    question: "With what is Loki able to control minds?",
+    options: [
+        "Sceptre",
+        "Mjolnir",
+        "Stick",
+        "Shield",
+    ],
+    answers: "Sceptre"
+},
+{
+    question: "Where was Captain America at the start of the movie?",
+    options: [
+        "Hospital",
+        "Aircraft",
+        "Gym",
+        "Home",
+    ],
+    answers: "Gym"
+},
+{
+    question: "What does Captain America bring to the fight?",
+    options: [
+        "His shield",
+        "Mjolnir",
+        "Sword",
+        "Bow & Arrow",
+    ],
+    answers: "His shield"
+},
+{
+    question: "What is the name of Thor's hammer?", 
+    options: [
+        "Jupiter",
+        "Hammer",
+        "Smasher",
+        "Mjolnir",
+    ],
+    answers: "Mjolnir"
+},
+{
+    question: "Who was the first Avenger?",
+    options: [
+        "Thor",
+        "Loki",
+        "Captian America",
+        "Iron Man",
+    ],
+    answer: "Captain America"
+},
+{
+    question: "Who attacked New York?",
+    options: [
+        "Thanos",
+        "Loki",
+        "Asgardians",
+        "Chitauri",
+    ],
+    answers: "Chitauri"
+},
+{
+    question: "Who was mind controlled by Loki?",
+    options: [
+        "Steve Rogers",
+        "Black Widow",
+        "Hawkeye",
+        "Nick Fury",
+    ],
+    answers: "Hawkeye"
+},
+{
+    question: "What is Black Widows name?",
+    options: [
+        "Pepper Potts",
+        "Natasha Romanoff",
+        "Nebula",
+        "Jane Foster",
+    ],
+    answers: "Natasha Romanoff"
+},
+{
+    question: "Who is the executive director of S.H.I.E.L.D?", 
+    options: [
+        "Nick Fury",
+        "Clint Barton",
+        "Bruce Banner",
+        "Erik Selvig",
+    ],
+    answers: "Nick Fury"
+},
+{
+    question: "How many Avengers are there?", 
+    options: [
+        "9",
+        "6",
+        "4",
+        "7",
+    ],
+    answers: "6"
+},
+{
+    question: "What is Hawkeye's speciality?",
+    options: [
+        "Swords",
+        "Nunchucks",
+        "Boomerang",
+        "Bow & Arrow",
+    ],
+    answer: "Bow & Arrow"
+},
+{
+    question: "Where does Natasha find Bruce?",
+    options: [
+        "China",
+        "London",
+        "India",
+        "Mexico",
+    ],
+    answers: "India"
+},
+{
+    question: "What is Tony Starks superpower?",
+    options: [
+        "Iron Man",
+        "Super strength",
+        "Super speed",
+        "Flying",
+    ],
+    answers: "Iron Man"
+},
+{
+    question: "Whose death resulted in a turning point for the team?",
+    options: [
+        "Pepper Potts",
+        "Phil Coulson",
+        "Nebula",
+        "Jane Foster",
+    ],
+    answers: "Phil Coulson"
+}
+];
