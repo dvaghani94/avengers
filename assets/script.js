9ab7376dbedbc67b1cbecfb1af196f04c107b329
Mohan Patel <mohan.s.patel@marriott.com>
2021-02-18 06:59:57 -0500

    added twitter to navbar

--- assets/script.js
+++ assets/script.js
@@ -19,19 +19,7 @@ function seacrhWindow(event) {
 }
 // searchBtn.addEventListener("click", seacrhWindow);
 
-// collapsible nav bar
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function myFunction() {
+  var element = document.body;
+    element.classList.toggle("dark-mode");
 }
