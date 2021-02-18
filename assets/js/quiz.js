474369358f766e6c1958905afa67cbb0cf957410
Jake Melville <jakemelville@ymail.com>
2021-02-18 13:29:48 -0500

    make some revisions to html, added js file with fetch. Created my header through postman.

---
+++ assets/js/quiz.js
@@ -0,0 +1,210 @@
+var startBtn = document.querySelector(“.btn-start”);
+var quiz = document.querySelector(“#quiz-section”);
+var quizStarter = document.querySelector(“.container”);
+var triviaQuestions = [ {
+    question: “Who does Hulk call ‘puny god’ while throwing them around like a rag doll?“,
+    options: [
+        {option: “Thor”, answer: false},
+        {option: “Loki”, answer: true},
+        {option: “Captian America”, answer: false},
+        {option: “Iron Man”, answer: false},
+    ],
+},
+{
+    question: “With what is Loki able to control minds?“,
+    options: [
+        {option: “Sceptre”, answer: true},
+        {option: “Mjolnir”, answer: false},
+        {option: “Stick”, answer: false},
+        {option: “Shield”, answer: false},
+    ],
+},
+{
+    question: “Where was Captain America at the start of the movie?“,
+    options: [
+        {option: “Hospital”, answer: false},
+        {option: “Aircraft”, answer: false},
+        {option: “Gym”, answer: true},
+        {option: “Home”, answer: false},
+    ],
+},
+{
+    question: “What does Captain America bring to the fight?“,
+    options: [
+        {option: “His shield”, answer: true},
+        {option: “Mjolnir”, answer: false},
+        {option: “Sword”, answer: false},
+        {option: “Bow & Arrow”, answer: false},
+    ],
+},
+{
+    question: “What is the name of Thor’s hammer?“,
+    options: [
+        {option: “Jupiter”, answer: false},
+        {option: “Hammer”, answer: false},
+        {option: “Smasher”, answer: false},
+        {option: “Mjolnir”, answer: true},
+    ],
+},
+{
+    question: “Who was the first Avenger?“,
+    options: [
+        {option: “Thor”, answer: false},
+        {option: “Loki”, answer: false},
+        {option: “Captian America”, answer: true},
+        {option: “Iron Man”, answer: false},
+    ],
+},
+{
+    question: “Who attacked New York?“,
+    options: [
+        {option: “Thanos”, answer: false},
+        {option: “Loki”, answer: false},
+        {option: “Asgardians”, answer: false},
+        {option: “Chitauri”, answer: true},
+    ],
+},
+{
+    question: “Who was mind controlled by Loki?“,
+    options: [
+        {option: “Steve Rogers”, answer: false},
+        {option: “Black Widow”, answer: false},
+        {option: “Hawkeye”, answer: true},
+        {option: “Nick Fury”, answer: false},
+    ],
+},
+{
+    question: “What is Black Widows name?“,
+    options: [
+        {option: “Pepper Potts”, answer: false},
+        {option: “Natasha Romanoff”, answer: true},
+        {option: “Nebula”, answer: false},
+        {option: “Jane Foster”, answer: false},
+    ],
+},
+{
+    question: “Who is the executive director of S.H.I.E.L.D?“,
+    options: [
+        {option: “Nick Fury”, answer: true},
+        {option: “Clint Barton”, answer: false},
+        {option: “Bruce Banner”, answer: false},
+        {option: “Erik Selvig”, answer: false},
+    ],
+},
+{
+    question: “How many Avengers are there?“,
+    options: [
+        {option: “9”, answer: false},
+        {option: “6", answer: true},
+        {option: “4”, answer: false},
+        {option: “7", answer: false},
+    ],
+},
+{
+    question: “What is Hawkeye’s speciality?“,
+    options: [
+        {option: “Swords”, answer: false},
+        {option: “Nunchucks”, answer: false},
+        {option: “Boomerang”, answer: false},
+        {option: “Bow & Arrow”, answer: true},
+    ],
+},
+{
+    question: “Where does Natasha find Bruce?“,
+    options: [
+        {option: “China”, answer: false},
+        {option: “London”, answer: false},
+        {option: “India”, answer: true},
+        {option: “Mexico”, answer: false},
+    ],
+},
+{
+    question: “What is Tony Starks superpower?“,
+    options: [
+        {option: “Iron Man”, answer: true},
+        {option: “Super strength”, answer: false},
+        {option: “Super speed”, answer: false},
+        {option: “Flying”, answer: false},
+    ],
+},
+{
+    question: “Whose death resulted in a turning point for the team?“,
+    options: [
+        {option: “Pepper Potts”, answer: false},
+        {option: “Phil Coulson”, answer: true},
+        {option: “Nebula”, answer: false},
+        {option: “Jane Foster”, answer: false},
+    ],
+}
+];
+startBtn.addEventListener(“click”, startQuiz);
+var quizQuestions = document.querySelector(“#question-text”);
+var answerChoice = document.querySelector(“#buttons-group”);
+function startQuiz() {
+    console.log(startBtn);
+   startBtn.classList.add(“hide”);
+   quizStarter.classList.add(“hide”);
+   randomQuestions = triviaQuestions.sort()
+   currentQuestion = 0;
+   quiz.classList.remove(“hide”);
+   setNext()
+}
+var nextBtn = document.querySelector(“.btn-next”)
+nextBtn.addEventListener(“click”, () => {
+    currentQuestion++
+    setNext()
+})
+function setNext() {
+    questionList(randomQuestions[currentQuestion]);
+    //resetQuestion()
+}
+function resetQuestion() {
+    nextBtn.classList.add(“hide”);
+    while (answerChoice.firstChild) {
+        answerChoice.removeChild;
+        (answerChoice.firstChild);
+    }
+}
+function questionList(question) {
+    quizQuestions.innerText = question.question;
+    question.options.forEach(option => {
+        console.log(option.option);
+        console.log(option.answer);
+        var optionBtn = document.createElement(“button”);
+        optionBtn.innerText = option.option;
+        optionBtn.classList.add(“btn”);
+        if (option.answer) {
+            optionBtn.dataset.answer = option.answer;
+        }
+        optionBtn.addEventListener(“click”, correctAnswer);
+        answerChoice.appendChild(optionBtn)
+    })
+}
+function correctAnswer(e) {
+    var userChoice = e.target
+    var correctAnswer = userChoice.dataset.answer;
+    setCorrectAnswer(document.body, answer);
+    Array.from(answerChoice.childen).forEach(button => {
+        setCorrectAnswer(button, button.dataset.answer)
+    })
+    if(randomQuestions.length > currentQuestion + 1) {
+        nextBtn.classList.remove(“hide”);
+    }
+    else {
+        startBtn.innerText = “Restart”;
+        startBtn.classList.remove(“hide”)
+    }
+}
+function setCorrectAnswer(element, answer) {
+    clearChoice(element);
+    if (answer) {
+        element.classList.add(“true”);
+    }
+    else {
+        element.classList.add(“false”);
+    }
+}
+function clearChoice(element) {
+    element.classList.remove(“true”);
+    element.classList.remove(“false”);
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,54 @@
-var getAvengers = () => {
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "OAuth oauth_consumer_key=\"RcmHKtrA030GlJA0nuoj10YTn\",oauth_token=\"270341803-oi0gMyN0m0zPWZIQExjqmzRAHFmqDHsBpALC9JGr\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1613669793\",oauth_nonce=\"6KHa8y48m9R\",oauth_version=\"1.0\",oauth_signature=\"k2kGpkfhxx201AI3CSujCZNe3nw%3D\"");
+myHeaders.append("Cookie", "guest_id=v1%3A161366419056994383; personalization_id=\"v1_nK6my6t1evj80ztdtJSraQ==\"");
 
-fetch(`http://www.omdbapi.com/?t=avengers&apikey=fbdde902`)
-  .then(res => res.json())
-  .then(posts => console.log(posts))
-}
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-var getIronMan = () => {
+fetch("https://cors-anywhere.herokuapp.com/https://api.twitter.com/2/tweets/search/recent?query=avengers", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
-fetch(`http://www.omdbapi.com/?t=iron+man&apikey=fbdde902`)
-  .then(res => res.json())
-  .then(posts => console.log(posts))
-}
 
-var getThor = () => {
 
-  fetch(`http://www.omdbapi.com/?t=thor&apikey=fbdde902`)
-    .then(res => res.json())
-    .then(posts => console.log(posts))
-  }
+// var getAvengers = () => {
 
-var getCaptainAmerica = () => {
+// fetch(`http://www.omdbapi.com/?t=avengers&apikey=fbdde902`)
+//   .then(res => res.json())
+//   .then(posts => console.log(posts))
+// }
 
-fetch(`http://www.omdbapi.com/?t=captain+america&apikey=fbdde902`)
-  .then(res => res.json())
-  .then(posts => console.log(posts))
-}
+// var getIronMan = () => {
 
-var getHulk = () => {
+// fetch(`http://www.omdbapi.com/?t=iron+man&apikey=fbdde902`)
+//   .then(res => res.json())
+//   .then(posts => console.log(posts))
+// }
 
-  fetch(`http://www.omdbapi.com/?t=hulk&apikey=fbdde902`)
-    .then(res => res.json())
-    .then(posts => console.log(posts))
-  }
+// var getThor = () => {
+
+//   fetch(`http://www.omdbapi.com/?t=thor&apikey=fbdde902`)
+//     .then(res => res.json())
+//     .then(posts => console.log(posts))
+//   }
+
+// var getCaptainAmerica = () => {
+
+// fetch(`http://www.omdbapi.com/?t=captain+america&apikey=fbdde902`)
+//   .then(res => res.json())
+//   .then(posts => console.log(posts))
+// }
+
+// var getHulk = () => {
+
+//   fetch(`http://www.omdbapi.com/?t=hulk&apikey=fbdde902`)
+//     .then(res => res.json())
+//     .then(posts => console.log(posts))
+//   }
 
 // var getBlackWidow = () => {
 
@@ -54,18 +71,20 @@ var getHulk = () => {
 //   .then(posts => console.log(posts))
 // }
 
-      
-
-getAvengers();
-getIronMan();
-getThor();
-getCaptainAmerica();
-getHulk();
 
 
+// getAvengers();
+// getIronMan();
+// getThor();
+// getCaptainAmerica();
+// getHulk();
 
 
 
-// Nmydu4qcPp5aIvJM6vQzsU7Wd    -twitter api key
-// vh5zGTEZg4zfo872GbKY9Ag9DUKqbXgDobAZSEEQkcnO3rIRQD twitter secret api key
+
+
+// 6KbkBqRDDdtNbMrLaovOHB7oy   -twitter api key
+// C5oTLEOjXt55ifu7JMC0PtjuNnRmHxLk70hSSMBIR5W1Ybo71N      twitter secret api key
 //  AAAAAAAAAAAAAAAAAAAAACZXMwEAAAAAIXtubGp2uDYSSKStFxUMcJrnPVQ%3Dyhg84UxwgwwSecQ55DS16LuAPYKwjl8EkdVvn4H8RcQj07yaDy  bearer token
+// 270341803-UpfkaG90lEsSzWDvHefJBIhBcBy1gM9EMWtfEs1F     access token
+//g2T6Yq0YXzTzkN90FGs58AdUS6RBJiZHovKxBF27oCN4W     access tokem secret
