380b91ce54be7e4f80d06d67179065e9a6311f33
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-02-18 15:19:39 -0500

    updating marvel.js

--- js/marvel.js
+++ js/marvel.js
@@ -1,26 +1,40 @@
 var characterId = "https://gateway.marvel.com:443/v1/public/characters/"
-var apiKey = "ac865cee692c9e8355597a42f5767d03"
+var apiKey = config.API_KEY_MARVEL
 
 var ironManId = "1009368"
 var captainAmericaId = "1009220"
 var hulkId = "1009351"
 var hawkeyeId = "1009338"
-var thorId ="1009664"
+var thorId = "1009664"
 var blackWidowId = "1009189"
 
 var hulkBio = document.getElementById("hulk")
+var thorBio = document.getElementById("thor")
 //hulkBio.value = "1"
 
-hulkBio.innerHTML = "1"
+hulkBio.innerHTML = ""
 
 console.log(hulkBio)
 
-fetch('https://gateway.marvel.com:443/v1/public/characters?ts=hulk&apikey=ac865cee692c9e8355597a42f5767d03&hash=1d43fe8dc58c7e2d777e5c7b82d544d4')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    hulkBio.innerHTML = data.data.results[0].name
-    console.log(data.data.results[0].name)
-    console.log(data);
-  });
+var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?ts=hulk&apikey=' + apiKey + '&hash=1d43fe8dc58c7e2d777e5c7b82d544d4";
+var hulkUrl = baseUrl + "&offset=520";
+var thorUrl = baseUrl + "&offset=700";
+var hulkIndex = 0;
+var thorIndex = 0;
+//Superhero=Hulk
+function getSuperHero(superHeroUrl, index) {
+
+  fetch('https://gateway.marvel.com:443/v1/public/characters?ts=hulk&apikey=' + apiKey + '&hash=1d43fe8dc58c7e2d777e5c7b82d544d4&offset=520')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.data.results[index]);
+      return data.data.results[index].description;
+      
+    });
+}
+
+hulkBio.innerHTML = getSuperHero(hulkUrl, hulkIndex)
+thorBio.innerHTML = getSuperHero(thorUrl, thorIndex)
+
--- js/ombd.js
+++ js/ombd.js
@@ -1,3 +1,31 @@
-var AvengersId = "http://www.omdbapi.com/?"
+const characterId = 'https://gateway.marvel.com/v1/public/characters?'
+const characterComic = 'https://gateway.marvel.com/v1/public/characters/'
+const apiKey = config.API_KEY_MARVEL
 
-var apiKey = fbdde902
+
+//hero's id numbers
+const thorId = '1009664'
+const hulkId = '1009351'
+const spiderManId = '1009610'
+const ironManId = '1009368'
+const wolverineId = '1009718'
+const thanosId = '1009652'
+
+
+const comicCover = document.querySelector(".comic-cover")
+const heroName = document.querySelector('.heroName')
+const heroDescription = document.querySelector('.heroDescription')
+var thorBio = document.getElementById("thor")
+
+
+function thor(){
+  const thor = 'ts=thor'
+  fetch(characterId + thor + '&apikey=' + apiKey)
+    .then(response => response.json())
+    .then(data => {
+
+      thorBio.innerHTML = data.data.results
+      //thorBio.innerHTML = data.data.results[0].description
+    })
+    console.log(data)
+};
