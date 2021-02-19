f0d47aeee1cec3c15332d23c7392fe7b5ca54ef1
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-02-19 09:33:07 -0500

    updated marvel.js

--- js/marvel.js
+++ js/marvel.js
@@ -1,40 +1,85 @@
 var characterId = "https://gateway.marvel.com:443/v1/public/characters/"
 var apiKey = config.API_KEY_MARVEL
+var hash = "55638210085726b38b41d0d6fe2afba7"
 
-var ironManId = "1009368"
-var captainAmericaId = "1009220"
-var hulkId = "1009351"
-var hawkeyeId = "1009338"
-var thorId = "1009664"
-var blackWidowId = "1009189"
+var ironManId = "1017104"
+var captainAmericaId = "1017105"
+var hulkId = "1017107"
+var hawkEyeId = "1017108"
+var thorId = "1017106"
+var blackWidowId = "1017109"
+var nickFuryId = "1009471"
 
-var hulkBio = document.getElementById("hulk")
-var thorBio = document.getElementById("thor")
-//hulkBio.value = "1"
 
-hulkBio.innerHTML = ""
+var thorBio = document.getElementById("thor");
+var captainAmericaBio = document.getElementById("captain-america");
+var hulkBio = document.getElementById("hulk");
+var blackWidowBio = document.getElementById("black-widow");
+var nickFuryBio = document.getElementById("nick-fury");
+var hawkEyeBio = document.getElementById("hawkeye");
+var ironManBio = document.getElementById("iron-man")
 
-console.log(hulkBio)
+fetch(characterId + thorId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    thorBio.innerHTML = data.data.results[0].description
+  });
 
-var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?ts=hulk&apikey=' + apiKey + '&hash=1d43fe8dc58c7e2d777e5c7b82d544d4";
-var hulkUrl = baseUrl + "&offset=520";
-var thorUrl = baseUrl + "&offset=700";
-var hulkIndex = 0;
-var thorIndex = 0;
-//Superhero=Hulk
-function getSuperHero(superHeroUrl, index) {
+fetch(characterId + hulkId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    hulkBio.innerHTML = data.data.results[0].description
+  });
 
-  fetch('https://gateway.marvel.com:443/v1/public/characters?ts=hulk&apikey=' + apiKey + '&hash=1d43fe8dc58c7e2d777e5c7b82d544d4&offset=520')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.data.results[index]);
-      return data.data.results[index].description;
-      
-    });
-}
+fetch(characterId + captainAmericaId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    captainAmericaBio.innerHTML = data.data.results[0].description
+  });
 
-hulkBio.innerHTML = getSuperHero(hulkUrl, hulkIndex)
-thorBio.innerHTML = getSuperHero(thorUrl, thorIndex)
+fetch(characterId + nickFuryId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    nickFuryBio.innerHTML = "A veteran S.H.I.E.L.D. operative, Nick Fury continues the legacy as one of the greatest super spies in the world."
+  });
+
+fetch(characterId + captainAmericaId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    captainAmericaBio.innerHTML = data.data.results[0].description
+  });
+
+fetch(characterId + blackWidowId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data) {
+    blackWidowBio.innerHTML = data.data.results[0].description
+  });
+
+fetch(characterId + hawkEyeId + "?ts=apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    hawkEyeBio.innerHTML = data.data.results[0].description
+  });
+
+fetch(characterId + ironManId + "?ts=1&apikey=" + apiKey + "&hash=" + hash)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    ironManBio.innerHTML = data.data.results[0].description
+  });
 
